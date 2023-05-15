1b67eaed15fa1476068b325b1f15be2453cd4f80
Nur Sakinah <nursakinah2402@gmail.com>
2023-05-15 22:45:50 +0700

    make view

---
+++ lib/view/kategoriBarang/kategori_barang.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../../controller/kategori_barang_controller.dart';
+import '../../model/kategori_barang_model.dart';
+import 'add_kategori_barang.dart';
+
+class KategoriBarang extends StatefulWidget {
+  const KategoriBarang({super.key});
+
+  @override
+  State<KategoriBarang> createState() => _KategoriBarangState();
+}
+
+class _KategoriBarangState extends State<KategoriBarang> {
+  final kategoriBarangController = BarangController();
+  List<KategoriBarangModel> listKategoriBarang = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getKategoriBarang();
+    super.initState();
+  }
+
+  void getKategoriBarang() async {
+    final kategoriBarang = await kategoriBarangController.getKategoriBarang();
+    setState(() {
+      listKategoriBarang = kategoriBarang;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Kategori Barang"),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: listKategoriBarang.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                  title: Text(listKategoriBarang[index].nama),
+                  trailing: IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.edit))),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const AddKategoriBarang()));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
